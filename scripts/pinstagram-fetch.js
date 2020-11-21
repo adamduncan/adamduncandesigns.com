ef9c63361e9beed21f7253cbc3729c9e5bc1f76b
Adam Duncan <mail@adamduncandesigns.com>
2020-11-21 18:45:46 +0000

    Update collection, API implementation
    New FB Graph API

--- scripts/pinstagram-fetch.js
+++ scripts/pinstagram-fetch.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 var fs = require("fs");
 var axios = require("axios");
 var chalk = require("chalk");
@@ -6,17 +8,17 @@ var collectionData = require("../pinstagram/collection.json");
 var collection = { items: [] };
 var dataObj = {};
 
-var getItemData = function() {
-  return collectionData.items.map(function(itemUrl) {
+var getItemData = function () {
+  return collectionData.items.map(function (itemUrl) {
     return axios
       .get(
-        "https://api.instagram.com/oembed/?url=" + itemUrl + "&omitscript=true"
+        `https://graph.facebook.com/v9.0/instagram_oembed?url=${itemUrl}&access_token=${process.env.IG_ACCESS_TOKEN}&omitscript=true`
       )
-      .then(function(res) {
+      .then(function (res) {
         console.log(chalk.green("Fetched: " + itemUrl));
         return res;
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.error(chalk.red("Error 404 fetching item: " + itemUrl));
       });
   });
@@ -27,7 +29,7 @@ console.log("Fetching data...");
 axios
   .all(getItemData())
   .then(
-    axios.spread(function(...args) {
+    axios.spread(function (...args) {
       dataObj.items = args
         // append URL property to each item
         .map((item, i) => {
@@ -37,13 +39,13 @@ axios
           }
         })
         // filter out items that have errored
-        .filter(item => item != undefined)
+        .filter((item) => item != undefined)
         // push successful items to updated collection
-        .map(item => {
+        .map((item) => {
           collection.items.push(item.data.url);
           return item;
         })
-        .map(item => {
+        .map((item) => {
           // clear out excessive embed html data
           item.data.html = "";
           return item.data;
@@ -54,7 +56,7 @@ axios
         "./pinstagram/collection.json",
         JSON.stringify(collection, null, 2),
         "utf8",
-        function(err) {
+        function (err) {
           if (err) throw err;
           console.log("404s removed. Updated pinstagram/collection.json");
         }
@@ -65,7 +67,7 @@ axios
         "./pinstagram/collectionFetched.json",
         JSON.stringify(dataObj),
         "utf8",
-        function(err) {
+        function (err) {
           if (err) throw err;
           console.log(
             "Data fetched. Written to pinstagram/collectionFetched.json"
@@ -74,6 +76,6 @@ axios
       );
     })
   )
-  .catch(function(error) {
+  .catch(function (error) {
     console.log(error);
   });
